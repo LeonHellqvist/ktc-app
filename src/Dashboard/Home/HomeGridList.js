31a68794ce1b5ab736767a372735d5c58f6380b6
LeonHellqvist <leon.norlin@outlook.com>
2021-08-26 22:38:56 +0200

    Lade till gruppering av lektioner efter tid

--- src/Dashboard/Home/HomeGridList.js
+++ src/Dashboard/Home/HomeGridList.js
@@ -7,21 +7,38 @@ export default function HomeGridList({ todaySchedule }) {
   useEffect(() => {
     if (!todaySchedule) return;
     var groups = [];
-    console.log(todaySchedule[0])
     for (var x = 0; x < todaySchedule.length; x++) {
-      for (var y = 0; y < groups.length; x++) {
-        if (
-          groups[y][0].blockName === todaySchedule[x].blockName &&
-          groups[y][0].timeStart === todaySchedule[x].timeStart &&
-          groups[y][0].timeEnd === todaySchedule[x]
-        ) {
-          groups[y].push([todaySchedule]);
-        } else {
-          groups.push(todaySchedule);
+      if (groups.length !== 0) {
+        for (var y = 0; y < groups.length; y++) {
+          var found = false;
+          if (
+            groups[y][0][0].timeStart === todaySchedule[x].timeStart &&
+            groups[y][0][0].timeEnd === todaySchedule[x].timeEnd
+          ) {
+            found = true;
+            groups[y][0].push(todaySchedule[x]);
+          }
         }
+        if (!found) {
+          groups.push([[todaySchedule[x]]]);
+        }
+      } else {
+        groups.push([[todaySchedule[0]]]);
       }
     }
-    console.log(groups)
+    for (var a = 1; a < groups.length; a++) {
+      if (
+        (groups[a][0][0].timeStartI < groups[a - 1][0][0].timeEndI &&
+          groups[a][0][0].timeStartI > groups[a - 1][0][0].timeStartI) ||
+        (groups[a][0][0].timeEndI < groups[a - 1][0][0].timeEndI &&
+          groups[a][0][0].timeEndI > groups[a - 1][0][0].timeStartI)
+      ) {
+        groups[a - 1].push(groups[a][0])
+        groups.splice(a, 1)
+        a--;
+      }
+    }
+    console.log(groups);
   }, [todaySchedule]);
 
   return <div></div>;
