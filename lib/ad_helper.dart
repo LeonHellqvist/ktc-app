27ec465ef94624fbb95d4b9c39e145d4d417a9e4
LeonHellqvist <leon@hellqvist.dev>
2023-02-08 17:32:42 +0100

    Added real AdUnits

--- lib/ad_helper.dart
+++ lib/ad_helper.dart
@@ -3,9 +3,9 @@ import 'dart:io';
 class AdHelper {
   static String get scheduleBannerAdUnit {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
+      return 'ca-app-pub-1670699619757926/2395173982';
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716';
+      return 'ca-app-pub-1670699619757926/6806369317';
     } else {
       throw UnsupportedError('Unsupported platform');
     }
@@ -13,9 +13,9 @@ class AdHelper {
 
   static String get foodBannerAdUnit {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
+      return 'ca-app-pub-1670699619757926/9024177425';
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716';
+      return 'ca-app-pub-1670699619757926/5480296465';
     } else {
       throw UnsupportedError('Unsupported platform');
     }
@@ -23,9 +23,9 @@ class AdHelper {
 
   static String get absentBannerAdUnit {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
+      return 'ca-app-pub-1670699619757926/4662260585';
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716';
+      return 'ca-app-pub-1670699619757926/1541051450';
     } else {
       throw UnsupportedError('Unsupported platform');
     }
