72b41021515071c3cff9e31e665d99719ea98605
LeonHellqvist <leon@hellqvist.dev>
2023-03-08 18:37:00 +0100

    Added week indicator

--- lib/food/food.dart
+++ lib/food/food.dart
@@ -74,61 +74,7 @@ class _FoodPageState extends State<FoodPage>
 
   @override
   Widget build(BuildContext context) {
-    final tabs = [
-      "1",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "11",
-      "12",
-      "13",
-      "14",
-      "15",
-      "16",
-      "17",
-      "18",
-      "19",
-      "20",
-      "21",
-      "22",
-      "23",
-      "24",
-      "25",
-      "26",
-      "27",
-      "28",
-      "29",
-      "30",
-      "31",
-      "32",
-      "33",
-      "34",
-      "35",
-      "36",
-      "37",
-      "38",
-      "39",
-      "40",
-      "41",
-      "42",
-      "43",
-      "44",
-      "45",
-      "46",
-      "47",
-      "48",
-      "49",
-      "50",
-      "51",
-      "52",
-      "53"
-    ];
+    final tabs = List.generate(53, (index) => "${index + 1}");
 
     return Scaffold(
       appBar: AppBar(
@@ -138,7 +84,14 @@ class _FoodPageState extends State<FoodPage>
           controller: _tabController,
           isScrollable: true,
           tabs: [
-            for (final tab in tabs) Tab(text: tab),
+            for (final tab in tabs)
+              tab == DateTime.now().weekOfYear.toString()
+                  ? Tab(
+                      child: Text(
+                          style: TextStyle(
+                              color: Theme.of(context).colorScheme.primary),
+                          tab))
+                  : Tab(text: tab)
           ],
         ),
       ),
